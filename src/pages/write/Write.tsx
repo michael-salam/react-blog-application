9fbf78bd97ea5cfbb54b822dd2fce43a30afeea8
Michael Salam <michaeldimimu@gmail.com>
2025-08-26 15:14:57 +0100

    Remove header from write article page

--- src/pages/write/Write.tsx
+++ src/pages/write/Write.tsx
@@ -4,18 +4,18 @@ import { SimpleEditor } from "../../components/tiptap-templates/simple/simple-ed
 
 const Write = () => {
   return (
-    <motion.main
-      initial={{ transform: "translateY(500px)", opacity: 0 }}
-      animate={{ transform: "translateY(0)", opacity: 1 }}
-    >
-      <header className="py-8">
-        <h1 className="heading heading-1 text-center mb-4">Write an article</h1>
-        <p className="mb-8 text-2xl max-w-[40ch] mx-auto">
-          Write your own articles and get your ideas out into the world
-        </p>
-      </header>
-      <SimpleEditor />
-    </motion.main>
+    // <motion.main
+    //   initial={{ transform: "translateY(500px)", opacity: 0 }}
+    //   animate={{ transform: "translateY(0)", opacity: 1 }}
+    // >
+    //   <header className="py-8">
+    //     <h1 className="heading heading-1 text-center mb-4">Write an article</h1>
+    //     <p className="mb-8 text-2xl max-w-[40ch] mx-auto">
+    //       Write your own articles and get your ideas out into the world
+    //     </p>
+    //   </header>
+    // </motion.main>
+    <SimpleEditor />
   );
 };
 
