d24e63b0829749e84953d58a500952ea63d9421f
Michael Salam <michaeldimimu@gmail.com>
2025-08-26 12:25:44 +0100

    Add page transition to profile page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,13 +1,18 @@
+import { motion } from "motion/react";
+
 const Profile = () => {
   return (
-    <main>
+    <motion.main
+      initial={{ transform: "translateY(500px)", opacity: 0 }}
+      animate={{ transform: "translateY(0)", opacity: 1 }}
+    >
       <header className="py-8">
         <h1 className="heading heading-1 text-center mb-4">
           Set up your profile
         </h1>
         <p className="mb-8 text-2xl">Get started in a few easy steps</p>
       </header>
-    </main>
+    </motion.main>
   );
 };
 
